b20e4076177f0d808d3c078ef21c47f04a7a616e
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-02 03:25:44 +0600

    update UI

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { FiMail, FiPhoneCall } from "react-icons/fi";
 import { Button, Text, SlideIn } from "../../components";
+import { MdOutlineLocationOn } from "react-icons/md";
 
 export const Contact = () => {
   return (
@@ -10,7 +11,10 @@ export const Contact = () => {
       id="contact"
     >
       <SlideIn direction={"left"} delay={0.1}>
-        <Text variant="heading" className={"mb-16 mobile:mb-4 mobile:text-2xl"}>
+        <Text
+          variant="heading"
+          className={"mb-16 text-6xl mobile:mb-4 mobile:text-2xl"}
+        >
           Contact Us
         </Text>
       </SlideIn>
@@ -51,33 +55,32 @@ export const Contact = () => {
           </SlideIn>
         </div>
 
-        <motion.div
-          whileHover={{ scale: [null, 1.5, 1.4] }}
-          transition={{ duration: 0.3 }}
-          className="bg-opacity-20 bg-primary-100 h-[40%] w-[40%] rounded-lg p-4 mobile:w-full mobile:h-full mobile:mt-8"
-        >
-          <Text variant="title" className={"text-white mobile:text-4xl"}>
+        <div className=" flex flex-col gap-5 bg-gradient-to-tr from-primary-100 via-purple-200 to-primary-100 h-[40%] w-[40%] rounded-lg p-4 mobile:w-full mobile:h-full mobile:mt-8">
+          <Text variant="title" className={"text-primary-400 mobile:text-4xl"}>
             Info
           </Text>
-          <div className="flex">
-            <FiMail
-              className={"inline mr-2 mobile:p-2"}
-              size={"40px"}
-              color="white"
-            />
-            <Text className={"text-3xl mobile:text-xl"}>
+          <div className="flex gap-2 ">
+            <FiMail className={"inline mr-2 mobile:p-2"} size={"40px"} />
+            <Text className={"text-primary-400 text-3xl mobile:text-xl"}>
               perceptron@gmail.com
             </Text>
           </div>
-          <div className="flex">
-            <FiPhoneCall
+          <div className="flex gap-2">
+            <FiPhoneCall className={"inline mr-2 mobile:p-2"} size={"40px"} />
+            <Text className={"text-primary-400 text-3xl mobile:text-xl"}>
+              +88-018-5998-1836
+            </Text>
+          </div>
+          <div className="flex gap-2">
+            <MdOutlineLocationOn
               className={"inline mr-2 mobile:p-2"}
               size={"40px"}
-              color="white"
             />
-            <Text className={"text-3xl mobile:text-xl"}>+88-018-5998-1836</Text>
+            <Text className={"text-primary-400 text-3xl mobile:text-xl"}>
+              +88-018-5998-1836
+            </Text>
           </div>
-        </motion.div>
+        </div>
       </div>
     </div>
   );
