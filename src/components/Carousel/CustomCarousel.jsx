65735433f41c5a14d4d0c23f2559d64198985177
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-03 00:08:20 +0600

    Refactor carousel component to improve performance and user experience

--- src/components/Carousel/CustomCarousel.jsx
+++ src/components/Carousel/CustomCarousel.jsx
@@ -16,76 +16,71 @@ export const CustomCarousel = () => {
 
   return (
     <div className="flex mobile:flex-col h-full">
-      <SlideIn direction={"left"}>
-        <Carousel
-          showArrows={true}
-          showStatus={false}
-          showThumbs={false}
-          autoPlay={true}
-          infiniteLoop={true}
-          transitionTime={1500}
-          interval={7000}
-          onChange={handleSlideChange}
-          className="w-[880px] 3xl:w-[1400px] h-full mr-8 mobile:w-[380px] mobile:mb-4"
-        >
-          {slides.map((slide) => (
-            <div key={slide.id}>
-              <Carousel
-                showArrows={false}
-                showStatus={false}
-                showThumbs={false}
-                autoPlay={true}
-                infiniteLoop={true}
-                transitionTime={500}
-                interval={2000}
+      <>
+        <SlideIn direction={"left"}>
+          <Carousel
+            showArrows={true}
+            showStatus={false}
+            showThumbs={false}
+            autoPlay={true}
+            infiniteLoop={true}
+            transitionTime={1000}
+            interval={4000}
+            onChange={handleSlideChange}
+            className="w-[880px] 3xl:w-[1400px] h-full mr-8 mobile:w-[380px] mobile:mb-4"
+          >
+            {slides.map((slide) => (
+              <div
+                key={slide.id}
+                className="inline-flex items-center justify-center p-1 h-full w-[880px] 3xl:w-[1400px] mobile:h-[250px] mobile:w-[380px] bg-gradient-to-r from-secondary-pink via-secondary-purple to-secondary-cyan rounded-xl"
               >
-                {slide.images.map((image, index) => (
-                  <div
-                    className="inline-flex items-center justify-center p-1 h-full w-[880px] 3xl:w-[1400px] mobile:h-[250px] mobile:w-[380px] bg-gradient-to-r from-secondary-pink via-secondary-purple to-secondary-cyan rounded-xl"
-                    key={index}
-                  >
-                    <img
-                      src={image}
-                      alt={slide.alt}
-                      className="rounded-xl object-cover h-full w-full"
-                    />
-                  </div>
-                ))}
-              </Carousel>
-            </div>
-          ))}
-        </Carousel>
-      </SlideIn>
+                <img
+                  src={slide.image}
+                  alt={slide.alt}
+                  className="rounded-xl object-cover h-full w-full"
+                />
+              </div>
+            ))}
+          </Carousel>
+        </SlideIn>
+      </>
 
-      <SlideIn direction={"left"}>
-        <section>
-          <Text
-            variant="heading"
-            className={"text-3xl mobile:text-2xl 3xl:text-4xl mb-2"}
-          >
-            {slides[currentSlide].title}
-          </Text>
-          <Text
-            variant="title"
-            className={
-              "text-white font-thin text-lg mobile:text-xl mobile:w-[96%] 3xl:text-3xl"
-            }
-          >
-            {slides[currentSlide].description}
-          </Text>
-        </section>
-        <section>
-          <div className="flex my-8">
-            <StatCard count={slides[currentSlide].days} text="Days Delivery" />
-            <StatCard
-              count={slides[currentSlide].satisfaction}
-              percent={true}
-              text="Performance Rate"
-            />
-          </div>
-          <Button variant={"primary"}>View Demo</Button>
-        </section>
-      </SlideIn>
+      <>
+        <SlideIn
+          className={"flex flex-col justify-between mobile:h-[60%] h-[80%]"}
+        >
+          <section>
+            <Text
+              variant="heading"
+              className={"text-3xl mobile:text-2xl 3xl:text-4xl mb-2"}
+            >
+              {slides[currentSlide].title}
+            </Text>
+            <Text
+              variant="title"
+              className={
+                " text-white font-thin text-lg mobile:text-xl mobile:w-[96%] 3xl:text-3xl"
+              }
+            >
+              {slides[currentSlide].description}
+            </Text>
+          </section>
+          <section>
+            <div className="flex my-8">
+              <StatCard
+                count={slides[currentSlide].days}
+                text="Days Delivery"
+              />
+              <StatCard
+                count={slides[currentSlide].satisfaction}
+                percent={true}
+                text="Customer Satisfaction"
+              />
+            </div>
+            <Button variant={"primary"}>View Demo</Button>
+          </section>
+        </SlideIn>
+      </>
     </div>
   );
 };
