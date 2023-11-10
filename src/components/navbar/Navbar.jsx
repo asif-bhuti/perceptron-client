f922b6a6b1f7bfa112cd4b53f6f37683b0035a7d
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-11 02:35:20 +0600

    Refactor navbar component with updated styles and links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,32 +29,36 @@ export const Navbar = () => {
           <MobileNavbar />
         </div>
       ) : (
-        <nav className={`z-10 relative`}>
-          <div className="w-full flex justify-between items-center">
+        <nav className={`z-10 relative px-4 py-2`}>
+          <div className=" flex justify-between items-center">
             <Link to="home" smooth={true}>
               <img
                 src="logo_nav.png"
                 alt="PERCEPTRON"
-                className="font-krona text-primary-100 m-6 text-[20px] h-8 cursor-pointer"
+                className="font-krona text-primary-100 text-[20px] h-8 cursor-pointer"
               />
             </Link>
-            <ul className="flex justify-end w-1/2 items-center">
-              <Link to={"home"} smooth={true} className="nav-link">
-                Home
+            <ul className="flex justify-end w-1/2 items-center ">
+              <Link to={"home"} smooth={true} className="nav-link-container">
+                <span className="relative">Home</span>
               </Link>
-              <Link to={"services"} smooth={true} className="nav-link">
-                Services
+              <Link
+                to={"services"}
+                smooth={true}
+                className="nav-link-container"
+              >
+                <span className="relative">Services</span>
               </Link>
-              <Link to={"projects"} smooth={true} className="nav-link">
-                Projects
+              <Link
+                to={"projects"}
+                smooth={true}
+                className="nav-link-container"
+              >
+                <span className="relative">Projects</span>
               </Link>
 
               <Button>
-                <Link
-                  to={"contact"}
-                  smooth={true}
-                  className="font-montserrat text-primary-100"
-                >
+                <Link to={"contact"} smooth={true} className="font-montserrat">
                   Contact
                 </Link>
               </Button>
