e8d0c3812f3339ca8b7eb07103dbabe820d950e2
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-11 16:58:45 +0600

    Update CustomCarousel component with new SlideIn and Text styles

--- src/components/Carousel/CustomCarousel.jsx
+++ src/components/Carousel/CustomCarousel.jsx
@@ -46,8 +46,8 @@ export const CustomCarousel = () => {
       </>
 
       <>
-        <SlideIn className={"flex flex-col justify-between h-[60%] sm:h-[80%]"}>
-          <section>
+        <SlideIn className={"flex flex-col h-[60%] sm:h-[80%]"}>
+          <section className="h-[80%] sm:h-[50%] 3xl:h-[40%]">
             <Text
               variant="heading"
               className={"sm:text-3xl text-2xl 2xl:text-4xl mb-2"}
